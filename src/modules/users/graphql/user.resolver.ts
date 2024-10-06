fa4b4e8e39db8e1fe94f035bd82f92bae843ca7d
poenaruiulian <poenaruiulian2003@gmail.com>
2024-10-06 23:43:28 +0300

    Returned user by email on query

--- src/modules/users/graphql/user.resolver.ts
+++ src/modules/users/graphql/user.resolver.ts
@@ -1,11 +1,13 @@
 import { Query, Resolver } from '@nestjs/graphql';
 import { User } from '../entities';
 import { RequestGraphql } from '../../../common';
+import { UsersService } from '../users.service';
 
 @Resolver(() => User)
 export class UserResolver {
+  constructor(private userService: UsersService) {}
   @Query(() => User)
-  async user(@RequestGraphql() req: any): Promise<User> {
-    return req.user;
+  async user(@RequestGraphql() req: any): Promise<User | null> {
+    return this.userService.findOne(req.user['email']);
   }
 }
