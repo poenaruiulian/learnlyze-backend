ab1812b8050c8ecb41dd0c48d004dd8c33b9af4c
poenaruiulian <poenaruiulian2003@gmail.com>
2025-01-02 18:36:36 +0200

    Implement resource replacement

---
+++ src/modules/resources/graphql/resources.resolver.ts
@@ -0,0 +1,23 @@
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Resource } from '../entities';
+import { ResourceService } from '../resources.service';
+import { RequestGraphql } from '../../../common';
+
+@Resolver(() => Resource)
+export class ResourcesResolver {
+  constructor(private resourceService: ResourceService) {}
+
+  @Mutation()
+  async replaceResource(
+    @RequestGraphql() req: any,
+    @Args({ name: 'stepId', type: () => Number }) stepId: number,
+    @Args({ name: 'resourceId', type: () => Number }) resourceId: number,
+    @Args({ name: 'feedback', type: () => String }) feedback: string,
+  ) {
+    return this.resourceService.replaceResource({
+      stepId,
+      resourceId,
+      feedback,
+    });
+  }
+}
--- src/modules/resources/resources.module.ts
+++ src/modules/resources/resources.module.ts
@@ -1,17 +1,20 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Resource } from './entities';
 import { ResourceService } from './resources.service';
 import { PuppeteerCore, PuppeteerModule } from 'nestjs-pptr';
+import { StepsModule, StepsService } from '../steps';
+import { ResourcesResolver } from './graphql/resources.resolver';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Resource]),
     PuppeteerModule.forRoot({ launchOptions: { headless: true } }),
     PuppeteerCore,
+    forwardRef(() => StepsModule),
   ],
   controllers: [],
-  providers: [ResourceService, PuppeteerCore],
+  providers: [ResourceService, PuppeteerCore, ResourcesResolver],
   exports: [ResourceService, PuppeteerCore],
 })
 export class ResourcesModule {}
