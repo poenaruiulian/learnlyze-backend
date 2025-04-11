7e243aedf64bd59cbb392241278d9137c4e0de61
poenaruiulian <poenaruiulian2003@gmail.com>
2025-04-11 12:58:01 +0300

    Update TypeORM configuration for Heroku with DATABASE_URL

--- src/config/database.module.ts
+++ src/config/database.module.ts
@@ -4,19 +4,34 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    // Make configuration available globally if needed
+    ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('POSTGRES_HOST'),
-        port: configService.get('POSTGRES_PORT'),
-        username: configService.get('POSTGRES_USER'),
-        password: configService.get('POSTGRES_PASSWORD'),
-        database: configService.get('POSTGRES_DB'),
-        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-        synchronize: true, // Be cautious about using synchronize in production
-      }),
+      useFactory: (configService: ConfigService) => {
+        const databaseUrl = configService.get<string>('DATABASE_URL');
+        if (databaseUrl) {
+          return {
+            type: 'postgres',
+            url: databaseUrl,
+            ssl: { rejectUnauthorized: false },
+            entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+            synchronize: false,
+          };
+        }
+
+        // Fallback for local development settings:
+        return {
+          type: 'postgres',
+          host: configService.get('POSTGRES_HOST'),
+          port: +configService.get<number>('POSTGRES_PORT'),
+          username: configService.get('POSTGRES_USER'),
+          password: configService.get('POSTGRES_PASSWORD'),
+          database: configService.get('POSTGRES_DB'),
+          entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+          synchronize: true,
+        };
+      },
       inject: [ConfigService],
     }),
   ],
